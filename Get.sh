b4e7b7ae5fc148b7715f7a1015998561a92c48a2
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-12-14 18:11:21 -0500

    Added user-friendly message
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- Get.sh
+++ Get.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+
+if ! test -f BaseSpace-IComLogin.txt
+then
+	echo "You must provide your Illumina BaseSpace cookie (the name of the cookie is IComLogin) in BaseSpace-IComLogin.txt"
+	exit
+fi
+
 for object in $(cat RawFiles.txt)
 do
 	# just take the part with fastq
 	fileName=$(for i in $(echo "$object"|sed 's=/= =g'|sed 's=?= =g'); do echo $i; done|grep fastq)
 
 	curl --output $fileName --continue-at - --location \
-		--cookie IComLogin=$(cat ~/123) "$object" &> $fileName.log &
+		--cookie IComLogin=$(cat BaseSpace-IComLogin.txt) "$object" &> $fileName.log &
 done
 
