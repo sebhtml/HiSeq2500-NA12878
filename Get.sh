6eab46a400429b13de3fbaa2efb0dc30f2c650a0
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-12-15 11:51:43 -0500

    Added 2 other samples
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- Get.sh
+++ Get.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-if ! test -f BaseSpace-IComLogin.txt
+if ! test $# = 2
 then
-	echo "You must provide your Illumina BaseSpace cookie (the name of the cookie is IComLogin) in BaseSpace-IComLogin.txt"
+	echo "Usage: $0 BaseSpace-Sample.txt BaseSpace-cookie.txt"
 	exit
 fi
 
-for object in $(cat RawFiles.txt)
+sample=$1
+cookie=$2
+
+if ! test -f $sample
+then
+	echo "You must provide your BaseSpace sample"
+	exit
+fi
+
+if ! test -f $cookie
+then
+	echo "You must provide your Illumina BaseSpace cookie (the name of the cookie is IComLogin)"
+	exit
+fi
+
+for object in $(cat $sample)
 do
 	# just take the part with fastq
 	fileName=$(for i in $(echo "$object"|sed 's=/= =g'|sed 's=?= =g'); do echo $i; done|grep fastq)
 
 	curl --output $fileName --continue-at - --location \
-		--cookie IComLogin=$(cat BaseSpace-IComLogin.txt) "$object" &> $fileName.log &
+		--cookie IComLogin=$(cat $cookie) "$object" &> $fileName.log &
 done
 
